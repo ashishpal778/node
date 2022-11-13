c5f2c9a9b49ea37d7d38d6fecab30aed2f5bd680
ashishpal778 <47516376+ashishpal778@users.noreply.github.com>
2022-11-14 02:06:36 +0530

    Add files via upload

--- 9_api2.js
+++ 9_api2.js
@@ -1,7 +1,21 @@
+//urls....
+//localhost:2000/signin
+// {
+//     "name": "molu",
+//     "email": "tet@examplle.com",
+//     "password": "12345"
+// }
+//localhost:2000/data
+
+// localhost:2000/signup
+
+
 //api using monggose(mongodb) and express(node js) js
 const express = require('express')
 // user =arun    //Password= a12345
 const app = express()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('./api2Models/userModel');
 const mongoose = require('mongoose');
 const { response } = require('express');
@@ -12,24 +26,99 @@ mongoose.connect(DB, {
 }).then(() => {
     console.log('Database connected...')
 })
-const hostname ='0.0.0.0'
-const PORT =process.env.PORT || 3000
+const hostname = '0.0.0.0'
+const PORT = process.env.PORT || 2000
 app.use(express.json()) // convert input bady data to json 
-app.listen(PORT,hostname ,() => {
+app.listen(PORT, hostname, () => {
     console.log(`Server is running on PORT ${PORT}`)
 })
-app.post('/signup', async (req, res) => {
-    let data = new User(req.body);
-    try {
-        let result = await data.save()
-        console.log(result)
-        res.send(result)
-    }
-    catch (err) {
-        res.status(500).send(err);
-    }
+
+app.post('/signup', (req, res, next) => {
+    console.log('post api running');
+    console.log(req.body.user)
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        console.log("hello", hash)
+        if (err) {
+            return res.ststus(500).json({
+                error: err
+            })
+        }
+        else {
+            let user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(result => {
+                    console.log(result)
+                    res.status(200)
+                    res.send(result)
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    })
+                })
+        }
+    })
 })
-app.get('/signin', async (req, res) => {
+
+// app.post('/signup', async (req, res) => {
+//     let data = new User(req.body);
+//     console.log(req.body.password)
+//     try {
+//         let result = await data.save()
+//         // console.log(result)
+//         res.send(result)
+//     }
+//     catch (err) {
+//         res.status(500).send(err);
+//     }
+// })
+app.post('/signin', (req, res, next) => {
+    console.log(req.body.email)
+    User.find({ email: req.body.email })
+        // .exec()
+        .then(user => {
+            console.log("mail",user)
+            if (user.length < 1) {
+                return res.status(4001).json({ msg: 'user not exist' })
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (!result) {
+                    return res.status(401).json({
+                        msg: 'password matching failed'
+                    })
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        username: user[0].name,
+                        usermail: user[0].email,
+
+                    },
+                        'this is dummy text',
+                        {
+                            expiresIn: '12000h'
+                        }
+                    );
+                    res.status(200).json({
+                        username: user[0].name,
+                        usermail: user[0].email,
+                        status :  200,
+                        token: token
+                    })
+                }
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+
+})
+app.get('/data', async (req, res) => {
     const data = await User.find({})
     try {
         res.send(data)
